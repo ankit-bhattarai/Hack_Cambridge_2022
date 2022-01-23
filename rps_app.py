5b588710dd197f4de752ebc5814b922c3fbd3dfc
Ankit Bhattarai <ankitbhattarai.ab@protonmail.com>
2022-01-23 11:59:49 +0000

    corn model upload

--- rps_app.py
+++ rps_app.py
@@ -3,37 +3,34 @@ import numpy as np
 from PIL import ImageOps, Image
 import pickle
 import cv2
+import tensorflow as tf
 
-model = pickle.load(open("file", "rb"))
+from keras.models import load_model
+model = load_model("model_corn_disease_selection")
 
 import streamlit as st
 
-st.write("App")
-st.write("Something")
+st.write("Determine Whether a plant leaf is healthy or not:")
 file = st.file_uploader("Please upload an image file", type=["jpg", "png"])
 
-labels = {0: 'apple', 1: 'banana', 2: 'beetroot', 3: 'bell pepper', 4: 'cabbage', 5: 'capsicum', 6: 'carrot',
-          7: 'cauliflower', 8: 'chilli pepper', 9: 'corn', 10: 'cucumber', 11: 'eggplant', 12: 'garlic', 13: 'ginger',
-          14: 'grapes', 15: 'jalepeno', 16: 'kiwi', 17: 'lemon', 18: 'lettuce',
-          19: 'mango', 20: 'onion', 21: 'orange', 22: 'paprika', 23: 'pear', 24: 'peas', 25: 'pineapple',
-          26: 'pomegranate', 27: 'potato', 28: 'raddish', 29: 'soy beans', 30: 'spinach', 31: 'sweetcorn',
-          32: 'sweetpotato', 33: 'tomato', 34: 'turnip', 35: 'watermelon'}
+labels = ['Healthy corn', 'Infected']
 
 
 def processed_img(img_data):
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    size = (224, 224)
+    data = np.ndarray(shape=(1, 180, 180, 3), dtype=np.float32)
+    size = (180, 180)
     image = ImageOps.fit(img_data, size, Image.ANTIALIAS)
     image_array = np.asarray(image)
     normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
 
     data[0] = normalized_image_array
     answer = model.predict(data)
-    y_class = answer.argmax(axis=-1)
-    y = " ".join(str(x) for x in y_class)
-    y = int(y)
-    res = labels[y]
-    return res.capitalize()
+    print(answer)
+    score = tf.nn.softmax(answer[0])
+    print(score)
+    txt = "{}".format(labels[np.argmax(score)])
+    print(txt)
+    return txt
 
 
 def main():
@@ -44,7 +41,7 @@ def main():
         st.image(image, use_column_width=True)
         prediction = processed_img(image)
 
-        st.write(prediction)
+        st.write("Conclusion: It is", prediction)
 
 
 if __name__ == "__main__":
