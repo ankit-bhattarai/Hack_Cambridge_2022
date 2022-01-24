cb9e5edc3ef0d55095fd8e58cae254acc3f2d88f
Ankit Bhattarai <ankitbhattarai.ab@protonmail.com>
2022-01-24 12:03:58 +0000

    web app update

--- rps_app.py
+++ rps_app.py
@@ -1,36 +1,31 @@
-from keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 from PIL import ImageOps, Image
-import pickle
-import cv2
 import tensorflow as tf
 
 from keras.models import load_model
+
 model = load_model("model_corn_disease_selection")
 
 import streamlit as st
 
-st.write("Determine Whether a plant leaf is healthy or not:")
+st.write("Determine Whether a plant leaf is healthy or not")
+st.write("The AI Model used was trained on images of corn leaf taken from this website:")
+st.write("https://www.kaggle.com/qramkrishna/corn-leaf-infection-dataset")
+st.write("This model works best with corn leaf images.")
 file = st.file_uploader("Please upload an image file", type=["jpg", "png"])
 
 labels = ['Healthy corn', 'Infected']
 
 
 def processed_img(img_data):
-    data = np.ndarray(shape=(1, 180, 180, 3), dtype=np.float32)
     size = (180, 180)
     image = ImageOps.fit(img_data, size, Image.ANTIALIAS)
-    image_array = np.asarray(image)
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-
-    data[0] = normalized_image_array
-    answer = model.predict(data)
-    print(answer)
-    score = tf.nn.softmax(answer[0])
-    print(score)
-    txt = "{}".format(labels[np.argmax(score)])
-    print(txt)
-    return txt
+    img_array = tf.keras.utils.img_to_array(image)
+    img_array = tf.expand_dims(img_array, 0)
+    predictions = model.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
+    return "Conclusion: This image is most likely {} with a {:.2f} percent confidence.".format(labels[np.argmax(score)],
+                                                                                               100 * np.max(score))
 
 
 def main():
@@ -39,9 +34,7 @@ def main():
     else:
         image = Image.open(file)
         st.image(image, use_column_width=True)
-        prediction = processed_img(image)
-
-        st.write("Conclusion: It is", prediction)
+        st.write(processed_img(image))
 
 
 if __name__ == "__main__":
